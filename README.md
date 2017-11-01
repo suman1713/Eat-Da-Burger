5b3bef23c94eff264e8004c6109c98567bbbc638
Suman1713 <sumn1713@gmail.com>
2017-10-31 22:58:53 -0500

    first upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Eat-Da-Burger
+Eat-Da-Burger is burger logger with MySQL, Node, Express, Handlebars and a homemade ORM.
---
+++ server.js
@@ -0,0 +1,36 @@
+//Dependencies
+var express = require("express");
+var methodOverride = require("method-override");
+var bodyParser = require("body-parser");
+
+var port = process.env.PORT || 3000;
+var app = express();
+
+//Middleware for express server
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+// Set Handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var mysql = require("mysql");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use("/", routes);
+
+app.listen(port, function() {
+  console.log("Listening on PORT " + port);
+});
+
+
